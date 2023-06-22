9e32c46ac1433719078ef398c8faaeb178260dce
kara <89068705+gongitsune@users.noreply.github.com>
2023-06-22 15:37:46 +0900

    bingo game

--- js1/k5/c5-1.c
+++ js1/k5/c5-1.c
@@ -102,12 +102,12 @@ bool open_card(int *card, int width, int num) {
 }
 
 int gen_rand_range(int min_val, int max_val) {
-  return (int)((rand() % (max_val + 1 - min_val)) + min_val);
+  return (int)(rand() % (max_val + 1 - min_val)) + min_val;
 }
 
 void shuffle(int *array, int size) {
   for (int i = 0; i < size; i++) {
-    int r = gen_rand_range(i, size);
+    int r = gen_rand_range(i, size - 1);
     int tmp = array[i];
     array[i] = array[r];
     array[r] = tmp;
@@ -124,14 +124,13 @@ int main(void) {
   srand((unsigned int)time(NULL));
   int rands[64];
   for (i = 1; i <= 64; i++) {
-    rands[i] = i;
+    rands[i - 1] = i;
   }
   shuffle(rands, 64);
   for (i = 0; i < size * size; i++) {
     bingo_num[i] = false;
     card[i] = rands[i];
   }
-  shuffle(card, size * size);
 
   int cnt = 0;
   print_card(card, size, bingo_num);
--- js1/k5/test.c
+++ js1/k5/test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int gen_rand_range(int min_val, int max_val) {
+  return (int)(rand() % (max_val + 1 - min_val)) + min_val;
+}
+void shuffle(int *array, int size) {
+  for (int i = 0; i < size; i++) {
+    int r = gen_rand_range(i, size - 1);
+    int tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+}
 
 int main(void) {
+  srand((unsigned int)time(NULL));
   int i;
-  int cnt = 0;
-  for (i = 0; i < 100; i++) {
-    cnt++;
-    i += 6;
+  int rands[64];
+  for (i = 0; i < 64; i++) {
+    rands[i] = i + 1;
   }
-
-  printf("%d\n", cnt);
   return 0;
 }
