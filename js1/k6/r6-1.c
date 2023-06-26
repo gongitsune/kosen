008fb7010a5f593396cd523bac49c9cdc7bee2c5
kara <89068705+gongitsune@users.noreply.github.com>
2023-06-26 09:52:11 +0900

    k6

---
+++ js1/k6/r6-1.c
@@ -0,0 +1,55 @@
+/* 23 豊島圭吾
+ * 第6回課題 プログラム1
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int a[10], tmp, index, i, j;
+
+  for (i = 0; i < 10; i++) {
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]);
+  }
+
+  for (i = 0; i < 9; i++) {
+    index = i;
+    for (j = i; j < 10; j++) {
+      if (a[j] > a[index]) {
+        index = j;
+      }
+    }
+    tmp = a[index];
+    a[index] = a[i];
+    a[i] = tmp;
+  }
+
+  for (i = 0; i < 10; i++) {
+    printf("%2d : %5d\n", i, a[i]);
+  }
+
+  return 0;
+}
+
+/* 実行結果
+a[0] = 10
+a[1] = 124
+a[2] = 12
+a[3] = 55
+a[4] = 14
+a[5] = 12
+a[6] = 5
+a[7] = 3
+a[8] = 7
+a[9] = 1
+ 0 :   124
+ 1 :    55
+ 2 :    14
+ 3 :    12
+ 4 :    12
+ 5 :    10
+ 6 :     7
+ 7 :     5
+ 8 :     3
+ 9 :     1
+ */
---
+++ js1/k6/r6-2.c
@@ -0,0 +1,56 @@
+/* 23 豊島圭吾
+ * 第6回課題 プログラム2
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int a[10], max, index, i, j;
+
+  for (i = 0; i < 10; i++) {
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]);
+  }
+
+  for (i = 0; i < 9; i++) {
+    max = a[i];
+    index = i;
+    for (j = i; j < 10; j++) {
+      if (a[j] > max) {
+        max = a[j];
+        index = j;
+      }
+    }
+    a[index] = a[i];
+    a[i] = max;
+  }
+
+  for (i = 0; i < 10; i++) {
+    printf("%2d : %5d\n", i, a[i]);
+  }
+
+  return 0;
+}
+
+/* 実行結果
+a[0] = 10
+a[1] = 124
+a[2] = 12
+a[3] = 55
+a[4] = 14
+a[5] = 12
+a[6] = 5
+a[7] = 3
+a[8] = 7
+a[9] = 1
+ 0 :   124
+ 1 :    55
+ 2 :    14
+ 3 :    12
+ 4 :    12
+ 5 :    10
+ 6 :     7
+ 7 :     5
+ 8 :     3
+ 9 :     1
+ */
