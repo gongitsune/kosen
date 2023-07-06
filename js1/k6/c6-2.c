9020d1cad7258d6a33e61a585cd616bfbbd4e428
kara <g22307@kagawa.kosen-ac.jp>
2023-07-06 15:01:55 +0900

    c6-3

--- js1/k6/c6-2.c
+++ js1/k6/c6-2.c
@@ -1,21 +1,25 @@
 /* 23 豊島圭吾
- * 第6回課題 プログラム2
+ * 挑戦的課題第6回 プログラム2
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
   int a[10], max, index, i, j;
 
+  srand((unsigned int)time(NULL));
   for (i = 0; i < 10; i++) {
-    printf("a[%d] = ", i);
-    scanf("%d", &a[i]);
+    a[i] = rand();
   }
 
+  int cnt = 0;
   for (i = 0; i < 9; i++) {
     max = a[i];
     index = i;
     for (j = i + 1; j < 10; j++) {
+      cnt++;
       if (a[j] > max) {
         max = a[j];
         index = j;
@@ -28,29 +32,7 @@ int main(void) {
   for (i = 0; i < 10; i++) {
     printf("%2d : %5d\n", i, a[i]);
   }
+  printf("count: %d\n", cnt);
 
   return 0;
 }
-
-/* 実行結果
-a[0] = 10
-a[1] = 124
-a[2] = 12
-a[3] = 55
-a[4] = 14
-a[5] = 12
-a[6] = 5
-a[7] = 3
-a[8] = 7
-a[9] = 1
- 0 :   124
- 1 :    55
- 2 :    14
- 3 :    12
- 4 :    12
- 5 :    10
- 6 :     7
- 7 :     5
- 8 :     3
- 9 :     1
- */
---
+++ js1/k6/c6-3.c
@@ -0,0 +1,45 @@
+/* 23 豊島圭吾
+ * 挑戦的課題第6回 プログラム3
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sort(int *a, int size) {
+  int i, x, j, cnt = 1;
+  for (i = 2; i <= size; i++) {
+    x = a[i];
+    j = i;
+    a[0] = x;
+    while (x > a[j - 1]) {
+      cnt++;
+      a[j] = a[j - 1];
+      j--;
+    }
+    a[j] = x;
+  }
+
+  return cnt;
+}
+
+void random_array(int *a, int size) {
+  int i;
+  for (i = 1; i < size; i++) {
+    a[i] = rand();
+  }
+}
+
+int main(void) {
+  int a[11], i, cnt_sum = 0;
+
+  srand((unsigned int)time(NULL));
+
+  for (i = 0; i < 100; i++) {
+    random_array(a, 11);
+    cnt_sum += sort(a, 11);
+  }
+  printf("count average: %lf\n", cnt_sum / 100.);
+
+  return 0;
+}
