11e0841a9772aca0352bce913ed34177dbe61f0f
kara <89068705+gongitsune@users.noreply.github.com>
2023-10-19 16:09:19 +0900

    c9

--- js1/k9/c9-3.c
+++ js1/k9/c9-3.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 int readdata(int a[]);
-void histogram(const int a[], int n);
+void histogram(const int a[], const int rank[], int n, const int light[]);
 void sort(int a[], int n);
 
 int main(void) {
@@ -30,22 +30,31 @@ int readdata(int a[]) {
   return n;
 }
 
-void histogram(const int a[], int n) {
+void histogram(const int a[], const int rank[], int n, const int light[]) {
   int i, r;
   for (i = 0; i < n; i++) {
-    printf("\033[Ka[%d]:", i);
+    printf("\033[Ka[%d]:", rank[i]);
+    if (light[i]) {
+      printf("\033[31m");
+    }
     for (r = 0; r < a[i]; r++) {
       printf("*");
     }
     printf("\n");
+    printf("\033[0m");
   }
 }
 
 void sort(int a[], int n) {
-  int max, index, i, j;
+  int max, index, i, j, light[n], r[n];
+
+  for (i = 0; i < n; i++) {
+    r[i] = i;
+    light[i] = 0;
+  }
 
   // 初期状態のヒストグラムを表示
-  histogram(a, n);
+  histogram(a, r, n, light);
 
   for (i = 0; i < n; i++) {
     max = a[i];
@@ -56,26 +65,39 @@ void sort(int a[], int n) {
         index = j;
       }
     }
+    j = r[index];
+    r[index] = r[i];
+    r[i] = j;
+
+    usleep(500 * 1000);
+    printf("\033[%dA", n);
+    light[index] = 1;
+    light[i] = 1;
+    histogram(a, r, n, light);
     a[index] = a[i];
     a[i] = max;
 
     usleep(500 * 1000);
-    // カーソル移動
     printf("\033[%dA", n);
-    histogram(a, n);
+    histogram(a, r, n, light);
+    light[index] = 0;
+    light[i] = 0;
   }
+
+  printf("\033[%dA", n);
+  histogram(a, r, n, light);
 }
 
 /* 実行結果
 n = 5
 a[0] = 7
-a[1] = 3
-a[2] = 5
+a[1] = 2
+a[2] = 8
 a[3] = 0
-a[4] = 2
+a[4] = 3
+a[2]:********
 a[0]:*******
-a[1]:***
-a[2]:*****
+a[4]:***
+a[1]:**
 a[3]:
-a[4]:**
 */
