b138f294f1ac929ac18a09d35cf76ff970a94725
kara <89068705+gongitsune@users.noreply.github.com>
2023-11-07 17:39:45 +0900

    k11

--- js1/k11/r11-2.c
+++ js1/k11/r11-2.c
@@ -3,5 +3,26 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
-int main(void) { char s[100], res[100]; }
+int main(void) {
+  char s[100], res[100];
+  int max_length = 0;
+  while (scanf("%s", s) != EOF) {
+    int cnt = strlen(s);
+    if (cnt > max_length) {
+      max_length = cnt;
+      strcpy(res, s);
+    }
+  }
+
+  printf("最も長い単語 = %s\n", res);
+  printf("その文字数 = %d\n", max_length);
+
+  return 0;
+}
+
+/* 実行結果
+最も長い単語 = embroidery-frame
+その文字数 = 16
+*/
---
+++ js1/k11/r11-3.c
@@ -0,0 +1,26 @@
+/* 23 豊島圭吾
+ * 第11回課題プログラム3
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  char s[100];
+  int n = 0;
+
+  printf("string = ");
+  scanf("%s", s);
+
+  while (s[n] != '\0') {
+    n++;
+  }
+
+  printf("文字数 = %d\n", n);
+
+  return 0;
+}
+
+/* 実行結果
+string = abcdefghijklmnopqrstuvwxyz
+文字数 = 26
+*/
